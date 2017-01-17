e3caa0e4b8a6a7fddfaa0ee703855ceb94fc1308
Maira <mllunaflores@gmail.com>
2017-01-17 11:30:07 -0600

    initial commit

---
+++ src/main/java/com/example/ServerStuffApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerStuffApplication.class, args);
+	}
+}
